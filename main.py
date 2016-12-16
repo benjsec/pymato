798c2b60e84be290cc1afcf5f0a5dea5913b6df5
Ben Secretan <git@benjsec.33mail.com>
2016-12-16 13:54:32 +0000

    Remove default phase list and improve displaying messages.
    The default phase list has been removed, and phases can no longer be
    passed into the constructor. Instead phases must be explicitly added
    once the Timer object has been created. This is with a view to providing
    a single method for adding phases which can be used easily when reading
    the configuration from a file.
    
    The displaying of messages has been refactored to make some use of the
    curses windows. Class methods have also been created to simplify the
    calls needed to update the strings, as well as to allow their position
    to be more easily updated in the future.

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import collections
 import curses
+# import logging
 import math
 import subprocess
 import time
 
 
+# def setup_log():
+#     """Create a logger, configure and return it."""
+#     logger = logging.getLogger('Timer')
+#     logger.setLevel(logging.DEBUG)
+#     formatter = logging.Formatter("[%(levelname)7s] %(message)s")
+#     handler = logging.FileHandler("timer.log")
+#     handler.setFormatter(formatter)
+#     logger.addHandler(handler)
+#     return logger, handler
+
+
+# log, handler = setup_log()       # pylint: disable=invalid-name
+
+
 Phase = collections.namedtuple("Phase", ["name", "duration"])
-
-
-phases = [Phase('working', 20*60), Phase('resting', 5*60)]
 
 
 class ExitException(Exception):
@@ -22,11 +34,13 @@ class Timer:
     def __init__(self, stdscr, phases=None):
         self.stdscr = stdscr
         self.stdscr.timeout(self.SECOND)
-        self.phases = phases or []
+        self.phases = []
         self.current_phase = None
-        self.stdscr.addstr(0, 0, " ~~ Pymato Timer ~~ ")
-        maxy, maxx = stdscr.getmaxyx()
-        self.stdscr.addstr(maxy-1, 0, "Space: Pause\t s: Skip phase\t q: Quit")
+        self.maxy, self.maxx = stdscr.getmaxyx()
+        self.stdscr.addstr(0, 0, " ~~ Pymato Timer ~~ ".center(self.maxx))
+        self.stdscr.addstr(self.maxy-1, 2,
+                           "Space: Pause\t s: Skip phase\t q: Quit")
+        self.win = self.stdscr.subwin(3, 0)
 
     def ding(self):
         p = subprocess.PIPE
@@ -35,19 +49,24 @@ class Timer:
             stdout=p, stderr=p)
 
     def add_phase(self, name, duration):
-        self.phases += Phase(name, duration)
+        phase = Phase(name, duration)
+        self.phases.append(phase)
 
     def end_phase(self):
         self.ding()
-        self.stdscr.addstr(
-            2, 0, "End of {} phase".format(self.current_phase).ljust(80))
-        self.stdscr.addstr(
-            3, 0, "Press any key to begin next phase.".ljust(80))
-        self.stdscr.addstr(4, 0, "".ljust(80))
-        self.stdscr.refresh()
+        self.win.cls()
+        self.disp_phase("End of {} phase", self.current_phase)
+        self.disp_time("Press any key to begin next phase.")
         self.wait_forever()
-        for i in range(2, 5):
-            self.stdscr.addstr(i, 0, "".ljust(80))
+        self.win.cls()
+
+    def disp_phase(self, fmt, *args):
+        self.win.addstr(0, 2, fmt.format(*args).ljust(self.maxx))
+        self.win.refresh()
+
+    def disp_time(self, fmt, *args):
+        self.win.addstr(2, 2, fmt.format(*args).ljust(self.maxx))
+        self.win.refresh()
 
     def wait_forever(self):
         self.stdscr.timeout(-1)
@@ -56,14 +75,20 @@ class Timer:
 
     def period(self, phase):
         self.current_phase = phase.name
-        self.stdscr.addstr(
-            2, 0, "You should currently be {}".format(phase.name).ljust(80))
+        self.disp_phase("You should currently be {}", phase.name)
         self.time(phase.duration)
 
     def pause(self):
-        self.stdscr.addstr(4, 0, "Timer paused".ljust(80))
-        self.stdscr.refresh()
+        self.disp_time("Timer paused, press SPACE to resume.")
         self.wait_forever()
+
+    def get_key(self):
+        try:
+            key = self.stdscr.getkey()
+        except curses.error:
+            return None
+        else:
+            return key.lower()
 
     def time(self, length):
         if length < 0:
@@ -71,31 +96,30 @@ class Timer:
                 "Length of phase must be positive, got {}".format(length))
         for i in range(length, 0, -1):
             m, s = math.floor(i/60), int(i%60)
-            self.stdscr.addstr(
-                4, 0, "Time remaining: {:0>2}:{:0>2}".format(m, s).ljust(80))
-            self.stdscr.refresh()
-            try:
-                key = self.stdscr.getkey().lower()
-            except curses.error:
-                pass
-            else:
-                if key == 'q':
-                    raise ExitException
-                elif key == 's':
-                    break
-                elif key == " ":
-                    self.pause()
+            self.disp_time("Time remaining: {:0>2}:{:0>2}", m, s)
+            # self.win.addstr(
+            #     2, 0, "Time remaining: {:0>2}:{:0>2}".format(m, s).ljust(80))
+            # self.win.refresh()
+            key = self.get_key()
+            if key == 'q':
+                raise ExitException
+            elif key == 's':
+                break
+            elif key == ' ':
+                self.pause()
         else:
             self.end_phase()
 
     def run(self, repeat=1):
         for x in range(repeat):
-            for phase in phases:
+            for phase in self.phases:
                 self.period(phase)
 
 
 def main(stdscr):
-    timer = Timer(stdscr, phases)
+    timer = Timer(stdscr)
+    timer.add_phase("working", 20*60)
+    timer.add_phase("resting", 5*60)
     timer.run(4)
 
 
@@ -103,4 +127,6 @@ if __name__ == "__main__":
     try:
         curses.wrapper(main)
     except (ExitException, KeyboardInterrupt):
+        pass
+    finally:
         pass
