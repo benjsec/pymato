e8971efa8c8748b2b0f243f801c9d804609e881c
Ben Secretan <git@benjsec.33mail.com>
2016-12-16 14:47:45 +0000

    Some refactoring and add comments to all methods
    All methods and classes now have at least a minimal comment.
    
    In the process of doing this some scope for refactoring improvements
    was found. This regarded simplifications to the waiting and key
    monitoring process, and has been fully implemented.

--- main.py
+++ main.py
@@ -27,7 +27,15 @@ class ExitException(Exception):
 
 
 class Timer:
+    """A timer class that encapuslates the main functionality of the program.
 
+    :param stdscr: The curses screen to display updates on.
+    """
+
+
+    # Currently the seconds are measured by waiting for the key presses, this
+    # may want to be updated to look at start and end time... but for now this
+    # works well enough.
     SECOND = 1000
 
     def __init__(self, stdscr):
@@ -42,61 +50,97 @@ class Timer:
         self.win = self.stdscr.subwin(3, 0)
 
     def ding(self):
+        """Make a "ding" noise"""
         p = subprocess.PIPE
         p1 = subprocess.Popen(
             ["ffplay", "-nodisp", "-autoexit", "complete.wav"],
             stdout=p, stderr=p)
 
     def add_phase(self, name, duration):
+        """Add a phase to the list of phases to cycle through.
+
+        :param name: The name of the phase, to be shown when in progress.
+        :param duration: The length of the phase, in seconds.
+        """
         phase = Phase(name, duration)
         self.phases.append(phase)
 
     def end_phase(self):
+        """To be called at the end of a phase.
+
+        This will display the end-of-phase message to the user, and wait for
+        a key to be pressed before continuing.
+        """
         self.ding()
         self.win.clear()
         self.disp_phase("End of {} phase", self.current_phase)
         self.disp_time("Press any key to begin next phase.")
-        self.wait_forever()
+        self.wait()
         self.win.clear()
 
     def disp_phase(self, fmt, *args):
+        """Update the line where the current phase is displayed.
+
+        :param fmt: A string format specification
+        :param args: Any arguments needed for the string formatting.
+        """
         self.win.addstr(0, 2, fmt.format(*args).ljust(self.maxx))
         self.win.refresh()
 
     def disp_time(self, fmt, *args):
+        """Update the line where the time remaining is displayed.
+
+        :param fmt: A string format specification
+        :param args: Any arguments needed for the string formatting.
+        """
         self.win.addstr(2, 2, fmt.format(*args).ljust(self.maxx))
         self.win.refresh()
 
-    def wait_forever(self):
-        self.stdscr.timeout(-1)
-        self.stdscr.getkey()
-        self.stdscr.timeout(self.SECOND)
+    def wait(self, timeout=-1):
+        """Wait until a key is pressed, or timeout, and return the key.
+
+        :param timeout: Time to wait for a key press in milliseconds. If not
+            given or -1 will wait forever.
+        :returns: The key pressed, or `None` if no key was pressed.
+        """
+        self.stdscr.timeout(timeout)
+        try:
+            key = self.stdscr.getkey()
+        except curses.error:
+            key = None
+        else:
+            key = key.lower()
+        finally:
+            self.stdscr.timeout(self.SECOND)
+        return key
 
     def period(self, phase):
+        """Begin a new phase period.
+
+        :param phase: The new phase period to move into. Must be a namedtuple
+            `Phase` object.
+        """
         self.current_phase = phase.name
         self.disp_phase("You should currently be {}", phase.name)
         self.time(phase.duration)
 
     def pause(self):
+        """Pause the timer until a key is pressed"""
         self.disp_time("Timer paused, press any key to resume.")
-        self.wait_forever()
-
-    def get_key(self):
-        try:
-            key = self.stdscr.getkey()
-        except curses.error:
-            return None
-        else:
-            return key.lower()
+        self.wait()
 
     def time(self, length):
+        """Time a length of seconds, and update the screen as it progresses.
+
+        :param length: The time in seconds to count for.
+        """
         if length < 0:
             raise ValueError(
                 "Length of phase must be positive, got {}".format(length))
         for i in range(length, 0, -1):
             self.disp_time("Time remaining: {}",
                 time.strftime("%M:%S", time.gmtime(i)))
-            key = self.get_key()
+            key = self.wait(self.SECOND)
             if key == 'q':  # Quit completely
                 raise ExitException
             elif key == 's':  # Skip phase
@@ -107,12 +151,17 @@ class Timer:
             self.end_phase()
 
     def run(self, repeat=1):
+        """Run the timer with the phases currently added.
+
+        :param repeat: The number of times to repeat the phases cycle.
+        """
         for x in range(repeat):
             for phase in self.phases:
                 self.period(phase)
 
 
 def main(stdscr):
+    """Create the timer, add the phases and run the timer."""
     timer = Timer(stdscr)
     timer.add_phase("working", 20*60)
     timer.add_phase("resting", 5*60)
